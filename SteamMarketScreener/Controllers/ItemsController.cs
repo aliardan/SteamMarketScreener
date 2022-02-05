bf7ad1afd784d19d9d387f1c45aba91d190f1e03
Aliardan <aliardanchik@gmail.com>
2022-02-05 23:01:09 +0300

    ItemsController implementation

---
+++ SteamMarketScreener/Controllers/ItemsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SteamMarketDAL;
+using SteamMarketDAL.Models;
+using System.ComponentModel;
+
+namespace SteamMarketScreener.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository _itemsRepository;
+
+        public ItemsController(IItemsRepository itemsRepository)
+        {
+            _itemsRepository = itemsRepository;
+        }
+
+        [HttpPost]
+        public ActionResult Add(Item item)
+        {
+            _itemsRepository.Add(item);
+
+            return new StatusCodeResult(200);
+        }
+
+        [HttpGet]
+        public ActionResult<Item> Get([DefaultValue("TestItemName")] string itemName)
+        {
+            var item = _itemsRepository.GetByName(itemName);
+
+            if (item == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return item;
+        }
+    }
+}
